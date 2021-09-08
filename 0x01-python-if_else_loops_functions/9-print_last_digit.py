c43568f399129584e95a2f3891c6e8a7c8ceb1e9
oangel26 <oa.angel26@gmail.com>
2021-09-07 23:33:15 -0500

    print format return int

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 def print_last_digit(number):
     """function that prints the last digit of a number."""
     num = str(number)
-    print("{}".format(num[-1]), end="")
+    print("{}".format(int(num[-1])), end="")
     return int(num[-1])
