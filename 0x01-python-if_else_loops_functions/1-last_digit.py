26e3dd51bf711aff28f05b63d024ff56a7c89423
oangel26 <oa.angel26@gmail.com>
2021-09-07 22:19:29 -0500

    Return intergers in place of string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,15 @@ import random
 number = random.randint(-10000, 10000)
 num = str(number)
 if int(num[-1]) == 0:
-    print("Last digit of {} is {} and is 0".format(number, num[-1]))
+    print("Last digit of {} is {} and is 0".format(number, int(num[-1])))
 elif int(num) < 0:
-    print("Last digit of {} is -{} and is less than 6 and not 0"
-          .format(num, num[-1]))
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, int(num[-1]) * -1))
 elif int(num[-1]) < 6:
     print("Last digit of {} is {} and is less than 6 and not 0"
-          .format(num, num[-1]))
+          .format(number, int(num[-1])))
 elif int(num[-1]) > 5:
     print("Last digit of {} is {} and and is greater than 5"
-          .format(num, num[-1]))
+          .format(number, int(num[-1])))
 else:
     pass
