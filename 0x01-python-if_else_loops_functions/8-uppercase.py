31acdb1921fb687f2481a4f75682e8c423ad6def
oangel26 <oa.angel26@gmail.com>
2021-09-08 11:43:09 -0500

    comments deleted

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Function that prints a string in uppercase followed by a new line."""
     for i in str:
         letter = i
         if ord(i) in range(97, 123):
