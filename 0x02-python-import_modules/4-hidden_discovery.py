827df5d56d09751cf845e0652a2dc449dff4bb1b
oangel26 <oa.angel26@gmail.com>
2021-09-08 20:05:19 -0500

    if __name__ == __name__ corrected

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Program that prints all the names defined by the compiled module"""
-if __name__ == "__name__":
+if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
         if i[0:2] != "__":
