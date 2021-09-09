c69e5653134a41d8b4f57b6e0c82b534629813be
oangel26 <oa.angel26@gmail.com>
2021-09-08 20:29:25 -0500

    comments deleted

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Program that imports functions form calculator_1.py and does some maths"""
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
