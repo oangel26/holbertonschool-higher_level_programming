ee94c5a2a13feb8c30226d9a2b46a9cb1058729d
oangel26 <oa.angel26@gmail.com>
2021-09-08 21:27:27 -0500

    number of argument condition error changed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
     from sys import argv, stdout
-    if len(argv) == 1:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+    if len(argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif len(argv) == 4:
         a = int(argv[1])
