53283296655b77ceae28a0cfa3681d90b0d4fbdc
oangel26 <oa.angel26@gmail.com>
2021-09-13 13:44:16 -0500

    str.format to print integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,7 @@
 def print_list_integer(my_list=[]):
     """Function that prints all integers of a list."""
     for i in my_list:
-        print(i)
+        print("{}".format(i))
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
