8bc2709e6d419d1129776c2cd388b15940d56cdf
oangel26 <oa.angel26@gmail.com>
2021-09-13 13:35:26 -0500

    First commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Function that prints all integers of a list."""
+    for i in my_list:
+        print(i)
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
