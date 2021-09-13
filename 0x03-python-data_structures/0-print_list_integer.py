b05cb988020bb6eebb7a916105dee68d50953a50
oangel26 <oa.angel26@gmail.com>
2021-09-13 13:49:11 -0500

    print interges {:d}

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """Function that prints all integers of a list."""
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
