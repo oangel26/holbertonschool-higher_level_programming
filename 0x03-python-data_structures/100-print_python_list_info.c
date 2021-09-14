8caac378fcf8304edc9a31446647c64c2e450dfa
oangel26 <oa.angel26@gmail.com>
2021-09-13 22:27:53 -0500

    a test

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - function that prints basic info about Python list
+ * @p: pointer to Python Object
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+	printf("*] Size of the Python List = %d\n", p->Py_SIZE);
+}
