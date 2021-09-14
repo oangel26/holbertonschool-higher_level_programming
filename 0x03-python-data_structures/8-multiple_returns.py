7bded764a2cf42885073fac74043a1737d7127ed
oangel26 <oa.angel26@gmail.com>
2021-09-13 19:22:34 -0500

    Condition for empty sentence added

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,13 +3,13 @@ def multiple_returns(sentence):
     """Function that returns a tuple with the length of a
     string and its first character.
     """
-    if sentence[0] is None:
-        sentence_tuple = len(sentence), None
+    if sentence == "":
+        sentence_tuple = 0, None
     else:
         sentence_tuple = len(sentence), sentence[0]
     return sentence_tuple
 
 if __name__ == "__main__":
-    sentence = "At Holberton school, I learnt C!"
+    sentence = ""
     length, first = multiple_returns(sentence)
     print("Length: {:d} - First character: {}".format(length, first))
