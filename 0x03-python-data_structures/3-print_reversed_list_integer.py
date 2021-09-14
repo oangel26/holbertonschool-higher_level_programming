43f54f479e4f236a1b3b2f6acfdaa9d996cb5b1d
oangel26 <oa.angel26@gmail.com>
2021-09-13 20:27:29 -0500

    if list is null, return None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """function that prints all integers of a list, in reverse order."""
-    if my_list is []:
+    if my_list == []:
         return None
     else:
         for i in reversed(my_list):
             print("{:d}".format(i))
 
 if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
+    my_list = []
     print_reversed_list_integer(my_list)
