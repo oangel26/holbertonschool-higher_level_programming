0067b5e09d28a0475c368c7b5b7d012e34ec04f4
oangel26 <oa.angel26@gmail.com>
2021-09-13 19:12:13 -0500

    First commit

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Function that finds all multiples of 2 in a list."""
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
+
+if __name__ == "__main__":
+    my_list = [0, 1, 2, 3, 4, 5, 6]
+    list_result = divisible_by_2(my_list)
+
+    i = 0
+    while i < len(list_result):
+        print("{:d} {:s} divisible by 2".format(my_list[i],
+                "is" if list_result[i] else "is not"))
+        i += 1
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """Function that finds the biggest integer of a list."""
+    if my_list == []:
+        return None
+    else:
+        max_n = 0
+        for i in my_list:
+            if max_n < i:
+                max_n = i
+        return max_n
+
+if __name__ == "__main__":
+    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+    max_value = max_integer(my_list)
+    print("Max: {}".format(max_value))
