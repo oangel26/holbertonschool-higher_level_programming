957d7ce64108d0192a62e6017ca84f4932be1505
oangel26 <oa.angel26@gmail.com>
2021-09-13 19:27:43 -0500

    varaible max_n initialized in first number of list instead of 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
     else:
-        max_n = 0
+        max_n = my_list[0]
         for i in my_list:
             if max_n < i:
                 max_n = i
