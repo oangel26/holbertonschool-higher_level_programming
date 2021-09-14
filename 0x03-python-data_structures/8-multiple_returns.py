0a900fa1d2f68ea8148cb3addfe998ed15d7e281
oangel26 <oa.angel26@gmail.com>
2021-09-13 19:14:26 -0500

    PEP8 correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,8 @@ def multiple_returns(sentence):
     """Function that returns a tuple with the length of a
     string and its first character.
     """
-    if sentence[0] == None:
-        sentence_tuple = 0, None
+    if sentence[0] is None:
+        sentence_tuple = len(sentence), None
     else:
         sentence_tuple = len(sentence), sentence[0]
     return sentence_tuple
