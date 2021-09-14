2ae0799a195064c126b8ad9e34df0c524a7c7051
oangel26 <oa.angel26@gmail.com>
2021-09-13 22:05:18 -0500

    code fixed in tuple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,18 +3,19 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """Function that adds 2 tuples"""
     if len(tuple_a) == 0:
         tuple_a = 0, 0
-    elif len(tuple_b) == 0:
+    if len(tuple_b) == 0:
         tuple_b = 0, 0
-    elif len(tuple_a) == 1:
-        tuple_a = tuple_a, 0
-    elif len(tuple_b) == 1:
-        tuple_b = tuple_b, 0
-    elif len(tuple_a) > 2:
+    if len(tuple_a) == 1:
+        tuple_a = tuple_a[0], 0
+    if len(tuple_b) == 1:
+        tuple_b = tuple_b[0], 0
+    if len(tuple_a) >= 2:
         tuple_a = tuple_a[0], tuple_a[1]
-    elif len(tuple_b) > 2:
+    if len(tuple_b) >= 2:
         tuple_b = tuple_b[0], tuple_b[1]
     new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return new_tuple
+
 
 if __name__ == "__main__":
     tuple_a = (1, 89)
