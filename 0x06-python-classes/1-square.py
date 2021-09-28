142111679fc435d7495f7fb330e40d67d1c6e98c
oangel26 <oa.angel26@gmail.com>
2021-09-28 17:16:31 -0500

    First commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Module which represents the model of a Square """
+
+
+class Square:
+    """ Class that defines the model of a square """
+    def __init__(self, size):
+        """ Constructor of the square """
+        self.__size = size
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
