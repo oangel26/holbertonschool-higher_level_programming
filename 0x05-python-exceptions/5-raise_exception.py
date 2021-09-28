63de69c31c41754ffb47ba32cf19b425c45622f8
oangel26 <oa.angel26@gmail.com>
2021-09-28 13:30:17 -0500

    Style correction

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -3,9 +3,9 @@ def raise_exception():
     """ Function that raises a type exception """
     raise TypeError
 
+
 if __name__ == "__main__":
     try:
         raise_exception()
     except TypeError as te:
         print("Exception raised")
-        
