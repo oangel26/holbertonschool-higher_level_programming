c68f32e6e9033647fde3d277b4d60c4c1f3414e2
oangel26 <oa.angel26@gmail.com>
2021-09-27 23:20:28 -0500

    First commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """ function that prints the first x elements
+    of a list and only integers
+    """
+    try:
+        count = 0
+        for i in range(x):
+            if type(my_list[i]) == int:
+                count += 1
+                print("{:d}".format(my_list[i]), end="")
+            else:
+                continue
+        print()
+        return(count)
+    except IndexError:
+        pass
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list_integers(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+
+    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+    nb_print = safe_print_list_integers(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
+    
