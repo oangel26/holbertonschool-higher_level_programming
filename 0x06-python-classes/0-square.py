fd34629daf976a14dbbf40a81e94699bb085d087
oangel26 <oa.angel26@gmail.com>
2021-09-28 16:40:25 -0500

    pycodestyle correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """ Module which represents the model of a Square """
+
+
 class Square:
     """ Class that creates the model of a square """
     pass
-
 
 if __name__ == "__main__":
     my_square = Square()
