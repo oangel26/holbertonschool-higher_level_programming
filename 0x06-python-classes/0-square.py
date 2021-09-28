c6749d240b144e916e5bd2fdb8f527e021a27de8
oangel26 <oa.angel26@gmail.com>
2021-09-28 16:16:31 -0500

    First commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """ Class that defines a square """
+    pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
