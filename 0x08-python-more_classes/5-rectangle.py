61129aaec179d1cdbc840a0d326259096a71cbc1
oangel26 <oa.angel26@gmail.com>
2021-09-30 23:00:31 -0500

    Raise errors corrected

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -25,10 +25,10 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if value < 0:
-            raise ValueError("width must be >= 0")
         if type(value) is not int:
             raise TypeError("width must be an integer")
+        elif type(value) is int and value < 0:
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -38,10 +38,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if value < 0:
-            raise ValueError("height must be >= 0")
         if type(value) is not int:
             raise TypeError("height must be an integer")
+        elif type(value) is int and value < 0:
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
