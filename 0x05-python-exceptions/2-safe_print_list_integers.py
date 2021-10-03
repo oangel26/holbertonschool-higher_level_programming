55a20cf31c4339d1e041f25e87df093e1fbc6295
oangel26 <oa.angel26@gmail.com>
2021-10-02 19:47:55 -0500

    return printed num

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             if my_list[i] != None and type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
-                count = 1
+                count += 1
             else:
                 continue
         print()
