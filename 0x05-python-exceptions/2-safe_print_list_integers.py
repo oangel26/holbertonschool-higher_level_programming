ff1ff17caa55f76612bc90d0a20448b6e1394f6c
oangel26 <oa.angel26@gmail.com>
2021-10-02 19:46:46 -0500

    raise exepton2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,20 +3,18 @@ def safe_print_list_integers(my_list=[], x=0):
     """ function that prints the first x elements
     of a list and only integers
     """
-    count = 0
-    for i in range(x):
-        count += 1
-        try:
-           if count <= x:
-               pass
-        except IndexError("list index out of range"):
-
-                if type(my_list[i]) is int:
-                    print("{:d}".format(my_list[i]), end="")
-                else:
-                    continue
-    print()
-    return (count)
+    try:
+        count = 0
+        for i in range(x):
+            if my_list[i] != None and type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count = 1
+            else:
+                continue
+        print()
+        return (count)
+    except Exception:
+        pass
 
 
 if __name__ == "__main__":
