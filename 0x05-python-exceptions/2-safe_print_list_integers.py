66fb027ace646fc604d15725d7e43dab274d6f46
oangel26 <oa.angel26@gmail.com>
2021-10-02 19:55:38 -0500

    Blank line EOF deleted

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """ function that prints the first x elements
-    of a list and only integers
-    """
     try:
         count = 0
         for i in range(x):
