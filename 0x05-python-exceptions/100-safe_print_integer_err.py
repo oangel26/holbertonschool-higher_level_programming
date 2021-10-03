02a002b1cca8e60b7de6d3407818a0781060e1b3
oangel26 <oa.angel26@gmail.com>
2021-10-02 20:26:05 -0500

    print to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print(e)
+        sys.stderr.write(str(e) + '\n')
         return False
 
 if __name__ == "__main__":
