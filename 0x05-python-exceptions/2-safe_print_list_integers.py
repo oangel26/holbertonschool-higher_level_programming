de69a9c0be71d1112fb6e4f1ae019ef95149e675
oangel26 <oa.angel26@gmail.com>
2021-10-04 10:14:06 -0500

    pycodestyle corretion

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,12 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
         for i in range(x):
-            if my_list[i] != None and type(my_list[i]) is int:
+            if my_list[i] is not None and type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
             else:
                 continue
         print()
         return (count)
-    except Exception:
-        pass
+    except Exception as e:
+        print(e)
