202d327d7c7a5845b649115fe114a3b7cac9920d
oangel26 <oa.angel26@gmail.com>
2021-10-13 17:11:55 -0500

    Function works when file has not being created

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 list_obj = []
+
 try:
+    my_list = load_from_json_file(filename)
+except Exception as e:
+    save_to_json_file(list_obj, filename)
+finally:
     my_list = load_from_json_file(filename)
     for i in range(1, len(argv)):
         my_list.append(argv[i])
     save_to_json_file(my_list, filename)
-except Exception as e:
-    save_to_json_file(list_obj, filename)
