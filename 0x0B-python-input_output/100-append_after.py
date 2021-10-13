f8671923cee813414a05da5d57dcf361c6a21bcd
oangel26 <oa.angel26@gmail.com>
2021-10-13 17:55:54 -0500

    append of first line deleted

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,10 +10,9 @@ def append_after(filename="", search_string="", new_string=""):
     file_lines = []
     with open(filename, 'r+', encoding="UTF8") as f:
         line = f.readline()
-        file_lines.append(line)
         while line:
             file_lines.append(line)
-            if  search_string in line:
+            if search_string in line:
                 file_lines.append(new_string)
             line = f.readline()
         f.seek(0, 0)
