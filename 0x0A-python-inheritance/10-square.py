9bfbd0b8e19b1f6a5babab46a143b383137b611c
oangel26 <oa.angel26@gmail.com>
2021-10-14 14:17:03 -0500

    fixed interger validator and import from outside

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,50 +1,6 @@
 #!/usr/bin/python3
 """ Module that represents class BaseGeometry """
-
-
-class BaseGeometry:
-    """ Class that represents the BaseGeometry """
-
-    def area(self):
-        """ Public instance method: that raises an Exception
-        with the message area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Public instance method that validates value
-
-        Args:
-        name: (str) name of integer
-        value: (int) integer to validate
-
-        Raises:
-        * If value is not an integer: raise a TypeError exception
-        * if value is less or equal to 0: raise a ValueError exception
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ Class that represents a Rectangle """
-
-    def __init__(self, width, height):
-        """ Constructor method of Rectangle class objects """
-        self.__height = height
-        self.__width = width
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-
-    def area(self):
-        """ Public instance method that return the area of the rectangle """
-        return (self.__width * self.__height)
-
-    def __str__(self):
-        """ return description: [Rectangle] <width>/<height> """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -53,8 +9,10 @@ class Square(Rectangle):
     def __init__(self, size):
         """ Constructor method of Square class objects """
         super().__init__(size, size)
-        super().integer_validator("size", size)
 
+    def integer_validator(self, name, value):
+        super().integer_validator("size", size)
+ 
     def area(self):
         """ Public instance method that return the area of a Square """
         return (super().area())
