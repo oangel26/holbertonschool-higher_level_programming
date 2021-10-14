fc591e6cd72226873c18bbae7bec6bcf8a388a99
oangel26 <oa.angel26@gmail.com>
2021-10-14 14:09:11 -0500

    Import BaseGeometry class from outside module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,31 +1,6 @@
 #!/usr/bin/python3
 """ Module that represents class BaseGeometry """
-
-
-class BaseGeometry:
-    """ Class that represents the BaseGeometry """
-
-    def area(self):
-        """ Public instance method: that raises an Exception
-        with the message area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Public instance method that validates value
-
-        Args:
-        name: (str) name of integer
-        value: (int) integer to validate
-
-        Raises:
-        * If value is not an integer: raise a TypeError exception
-        * if value is less or equal to 0: raise a ValueError exception
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -40,10 +15,7 @@ class Rectangle(BaseGeometry):
 
 
 if __name__ == "__main__":
-
-
     print(issubclass(Rectangle, BaseGeometry))
-
 
     r = Rectangle(3, 5)
 
