dab0d7f439fc43b08f5f4a2e1a2afb6a8e4cda32
oangel26 <oa.angel26@gmail.com>
2021-10-14 13:22:03 -0500

    False return added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -16,7 +16,8 @@ def is_same_class(obj, a_class):
     if (isinstance(obj, a_class)):
         if (obj.__class__ is a_class):
             return (True)
-
+        else:
+            return (False)
 
 if __name__ == "__main__":
     a = 1
@@ -26,3 +27,22 @@ if __name__ == "__main__":
         print("{} is an instance of the class {}".format(a, float.__name__))
     if is_same_class(a, object):
         print("{} is an instance of the class {}".format(a, object.__name__))
+    a = True
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, object.__name__))
+    a = 3.14
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, object.__name__))
+    a = True
+    if is_same_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))
+    a = None
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, object.__name__))
+    a = None
+    if is_same_class(a, list):
+        print("{} is an instance of the class {}".format(a, object.__name__))
+    a = [1, 2, 3]
+    if is_same_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))
+    
