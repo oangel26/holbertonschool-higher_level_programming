63f8afdc175b720768b5cea9c3f2ff971d7785b9
oangel26 <oa.angel26@gmail.com>
2021-10-15 00:38:29 -0500

    initialization of size

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 """ Module that represents class BaseGeometry """
 Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Square(Rectangle):
+class Square(Rectangle, BaseGeometry):
     """ Class that represents a square """
 
     def __init__(self, size):
         """ Constructor method of Square class objects """
-        super().__init__(size, size)
+        self.__size = size
+        super().integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
 
+
+    """
     def integer_validator(self, name, value):
-        super().integer_validator("size", size)
+        Public instance method that validates value
 
+    if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+    """
     def area(self):
         """ Public instance method that return the area of a Square """
         return (super().area())
@@ -20,6 +30,5 @@ class Square(Rectangle):
 
 if __name__ == "__main__":
     s = Square(13)
-
+    print(s.__dict__)
     print(s)
-    print(s.area())
