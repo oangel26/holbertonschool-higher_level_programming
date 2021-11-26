f6c7c907b9b02a6174303007f4265669fdfb2130
oangel26 <oa.angel26@gmail.com>
2021-11-25 20:13:40 -0500

    join cities.state_id = states.id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,6 +2,6 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 SELECT DISTINCT cities.id, cities.name, states.name
-FROM cities
-	 NATURAL JOIN states
+FROM cities, states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
