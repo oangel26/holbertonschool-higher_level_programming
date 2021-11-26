f19b4f4b8818d326503cf909fcfc7929dfb99de6
oangel26 <oa.angel26@gmail.com>
2021-11-25 20:01:42 -0500

    ASC ORDER Added

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
-SELECT cities.id, cities.name, states.name;
+SELECT cities.id, cities.name, states.name
+ORDER BY cities.id ASC;
