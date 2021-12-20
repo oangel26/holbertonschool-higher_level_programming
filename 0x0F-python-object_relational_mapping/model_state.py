734fa30d1a7ae2f614054d13193afd08a5b4bd66
oangel26 <oa.angel26@gmail.com>
2021-12-19 22:29:49 -0500

    autoincrement added

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,5 @@ class State(Base):
 
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-
