88d3c6fcb00d9a56be4fee38bde084b21c78aee3
oangel26 <oa.angel26@gmail.com>
2021-12-19 23:40:00 -0500

    correction

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -15,4 +15,4 @@ class City(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
-
+    state = relationship("State", back_populates="cities")
