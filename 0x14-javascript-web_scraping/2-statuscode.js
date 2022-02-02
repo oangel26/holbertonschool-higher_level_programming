42930157361a656f8fddbde8b8dbed5f40d1d161
oangel26 <oa.angel26@gmail.com>
2022-02-01 21:09:28 -0500

    [FIX] error handling and status code messsage

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,12 +3,10 @@
 Script that display the status code of a GET request
 */
 
-const process = require('process');
 const args = process.argv;
-
-const requestURL = args[2];
-
 const request = require('request');
-request(requestURL, function (response) {
-  console.log('statusCode:', response.statusCode);
+
+request(args[2], function (err, response) {
+  if (err) throw err;
+  console.log('code:', response && response.statusCode);
 });
