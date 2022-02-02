d4391309dda407475a701db86760b9c7c8f3518a
oangel26 <oa.angel26@gmail.com>
2022-02-01 19:53:23 -0500

    First commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/local/bin/node
+/*
+script that reads and prints the content of a file.
+*/
+
+const process = require('process');
+const fs = require('fs');
+
+const args = process.argv;
+
+try {
+  const data = fs.readFileSync(args[2], 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
