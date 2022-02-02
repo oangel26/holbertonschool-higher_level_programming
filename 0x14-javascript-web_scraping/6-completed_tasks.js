fb0b383adb259f7f407a0a5985230b2343fef1f2
oangel26 <oa.angel26@gmail.com>
2022-02-01 23:27:13 -0500

    Firts commit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+script that computes the number of tasks completed by user id
+*/
+
+const args = process.argv;
+const request = require('request');
+
+request(args[2], function (err, request) {
+  if (err) throw err;
+  const data = JSON.parse(request.body);
+  const dict = {};
+  data.forEach(todos => {
+    if (todos.completed) {
+      if (dict[todos.userId]) {
+        dict[todos.userId] += 1;
+      } else {
+        dict[todos.userId] = 1;
+      }
+    }
+  });
+  console.log(dict);
+});
