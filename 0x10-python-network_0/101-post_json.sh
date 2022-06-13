80c8fff6765cbed0b2ab776c7af9c466c7281c74
oangel26 <oa.angel26@gmail.com>
2022-06-13 18:12:49 -0500

    Exe permits

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a JSON POST request to a URL passed as the first argument
-curl -H "Content-Type: application/json" -d "$2" "$1"
+curl -sH "Content-Type: application/json" -d @"$2" "$1"
