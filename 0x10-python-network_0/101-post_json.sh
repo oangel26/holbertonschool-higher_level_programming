2f57908b69423a75c0e62a37056ecf9f2681dc6d
oangel26 <oa.angel26@gmail.com>
2022-06-13 18:10:51 -0500

    First commit

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends a JSON POST request to a URL passed as the first argumen
-curl -so /dev/null -sw "%{http_code}" "$1"
+# script that sends a JSON POST request to a URL passed as the first argument
+curl -H "Content-Type: application/json" -d "$2" "$1"
