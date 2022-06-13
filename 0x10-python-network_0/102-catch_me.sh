7dc6f57192df59e29d12d0e9ed63006c88cb758a
oangel26 <oa.angel26@gmail.com>
2022-06-13 18:17:24 -0500

    Fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server
-curl -sLX PUT -d "user_id=98" -H "You got me!" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin: You got me!" 0.0.0.0:5000/catch_me
